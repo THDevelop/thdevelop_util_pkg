5e6f3bdc3ff5d07af9d0b0b4e71bfa6012346c52
THDevelop <develop_t@hotmail.com>
2018-08-07 07:02:24 +0200

    add function for pdf preview

--- thdevelop_util_pkg.sql
+++ thdevelop_util_pkg.sql
@@ -16,6 +16,22 @@
                            )
   return varchar2; 
 
+  /**
+  * Get the PDF File from the Database to the application
+  * @param pi_pdf_id_column name of the pk column
+  * @param pi_filename_column name of the filename column
+  * @param pi_bolb_column name of the blob column
+  * @param pi_tablename name of the table where the blobs (PDF) is save
+  * @param pi_pdf_id PK of the pdf that will be render in the region
+  */
+  procedure get_preview(pi_pdf_id_column in varchar2,
+                        pi_filename_column in varchar2,
+                        pi_bolb_column in varchar2,
+                        pi_tablename in varchar2,
+                        pi_pdf_id in number,
+                        pi_sql_statement in varchar2 default null
+                       );
+
   end thdevelop_util_pkg;
   /
 
@@ -58,5 +74,49 @@
     return l_return;
   end apex_item_button; 
 
+  procedure get_preview(pi_pdf_id_column in varchar2,
+                        pi_filename_column in varchar2,
+                        pi_bolb_column in varchar2,
+                        pi_tablename in varchar2,
+                        pi_pdf_id in number,
+                        pi_sql_statement in varchar2 default null
+                      )
+  as
+    l_file blob;
+    l_filename varchar2(4000);
+    l_sql varchar2(4000);
+  begin
+
+    l_sql := 'select ' || dbms_assert.simple_sql_name(pi_filename_column) ||
+             '     , ' || dbms_assert.simple_sql_name(pi_bolb_column) ||
+             '  from ' || dbms_assert.sql_object_name(pi_tablename) ||
+             ' where ' || dbms_assert.simple_sql_name(pi_pdf_id_column) || ' = :pdf_id'
+    ;
+   EXECUTE IMMEDIATE l_sql
+                 INTO l_filename,
+                      l_file
+                USING pi_pdf_id;  
+
+
+  sys.htp.init;
+  sys.owa_util.mime_header( 'application/pdf', false );
+  sys.htp.p('Content-length: ' || sys.dbms_lob.getlength( l_file));
+  sys.htp.p('Content-Disposition: inline; filename="' || l_filename ||
+  '"' );
+  sys.owa_util.http_header_close;
+  sys.wpg_docload.download_file( l_file );
+
+  apex_application.stop_apex_engine;
+  exception
+  when others then
+  raise;
+  end get_preview;
+
   end thdevelop_util_pkg ;
   /
+
+  thdevelop_util_pkg.get_preview(pi_pdf_id_column => 'PK_ID',
+                                 pi_filename_column => 'FILENAME',
+                                 pi_bolb_column => 'FILE_CONTENT',
+                                 pi_tablename => 'PDF_REGION',
+                                 pi_pdf_id => :P3_REPORTID);
